62bdf17f4417cc41358019f9bfb41d50dd982b7d
Olly Seber <olly_s@hotmail.co.uk>
2022-07-06 13:02:08 -0700

    Implement basic functionality

--- script.rb
+++ script.rb
@@ -1,3 +1,16 @@
 def caesar_cipher(string, shift)
-    
+    encoded = ""
+    string.each_char do |char|
+        num = char.ord
+        if (char.between?("a", "z") || char.between?("A", "Z"))
+            puts char.ord
+            num += shift
+            puts num
+        end
+        encoded += num.chr
+        puts num.chr
+    end
+    encoded
 end
+
+puts caesar_cipher("What a string!", 5)
