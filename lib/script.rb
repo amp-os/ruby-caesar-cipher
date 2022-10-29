50254e4758d574c4ae638677983b7a39ec1f40f0
Olly Seber <olly_s@hotmail.co.uk>
2022-10-28 18:09:37 -0700

    Improve formatting

--- lib/script.rb
+++ lib/script.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, shift)
-    result = ""
-    string.each_char do |char|
-        if (char.between?("a", "z") || char.between?("A", "Z"))
-            code = char.ord
-            char_shift_offset = char <= "Z" ? 65 : 97
-            code = ((code - char_shift_offset + shift) % 26) + char_shift_offset
-            char = code.chr
-        end
-        result += char
+  result = ""
+  string.each_char do |char|
+    if char.between?("a", "z") || char.between?("A", "Z")
+      code = char.ord
+      char_shift_offset = char <= "Z" ? 65 : 97
+      code = ((code - char_shift_offset + shift) % 26) + char_shift_offset
+      char = code.chr
     end
-    result
+    result += char
+  end
+  result
 end
 
 puts caesar_cipher("dCode Caesar", -166)
